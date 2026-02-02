6f642680fb3aa6e2e996ffb08065b69a33cf2762
anst_dud <dudinaa964@gmail.com>
2026-02-02 11:27:34 +0300

    Фикс лого

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Hero from './components/Hero/Hero.jsx';
 import Services from './components/Services/Services.jsx';
@@ -8,7 +7,7 @@ import Contacts from './components/Contacts/Contacts.jsx';
 import Process from "./components/Process/Process.jsx";
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy.jsx';
 import Footer from "./components/Footer/Footer.jsx";
-import NotFound from './components/NotFound/NotFound.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
@@ -27,8 +26,8 @@ function App() {
                         <Footer />
                     </div>
                 } />
+
                 <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
